1fd055d97690329ef11a3015ee551d4563296e47
AnnaSvalova <saa1330@gmail.com>
2014-03-26 19:47:35 +0600

    Домашнее задание 3. Свалова Анна
    Попробовала реализовать сессии. Вроде работает.

--- appMVC.py
+++ appMVC.py
@@ -14,7 +14,7 @@ __author__ = 'pahaz'
 #
 # ===========================
 
-from cgi import escape
+
 from urlparse import parse_qs
 
 
@@ -22,9 +22,15 @@ def http_status(code):
     return "200 OK" if code == 200 else "404 Not Found"
 
 
+def create_response_headers(sid=''):
+    response_headers = [('Content-Type', 'text/html')]
+    if sid:
+        response_headers += [('Set-Cookie', 'sid=' + sid + '; path=/; expires=Thu, 1 Jan 2015 23:59:59 GMT')]
+    return response_headers
+
 # ===========================
 #
-#           Model
+#           Models
 #
 # ===========================
 
@@ -52,11 +58,42 @@ class TextModel(object):
         self._db.sync()
 
 
+import time
+import hashlib
+
+
+class SessionModel(object):
+    SESSION_DB = "sess.db"
+
+    def __init__(self):
+        self.db = shelve.open(self.SESSION_DB)
+
+    def add(self):
+        sid = hashlib.md5(str(time.time())).hexdigest()
+        self.db[sid] = {
+            "amount_of_viewed_pages": 0,
+            "is_auth": 0
+        }
+        self.db.sync()
+        return sid
+
+    def get(self, sid, default_value=''):
+        return self.db.get(sid, default_value)
+
+    def set(self, sid, user_stat):
+        self.db[sid] = user_stat
+        self.db.sync()
+
+    def delete(self, sid):
+        del self.db[sid]
+        self.db.sync()
+
 # ===========================
 #
 #   Controller and Router
 #
 # ===========================
+
 
 class Router(object):
     def __init__(self):
@@ -66,33 +103,61 @@ class Router(object):
         path = environ['PATH_INFO']
         query_dict = parse_qs(environ['QUERY_STRING'])
 
+        if 'HTTP_COOKIE' in environ:
+            cookie = parse_qs(environ['HTTP_COOKIE'])
+            sid = cookie.get('sid')[0]
+            if (not sess.get(sid)):
+                sid = sess.add()
+                response_headers = create_response_headers(sid)
+            else:
+                response_headers = create_response_headers()
+        else:
+            sid = sess.add()
+            response_headers = create_response_headers(sid)
+
         if path in self._paths:
-            res = self._paths[path](query_dict)
+            if path == '/text' or path == '/text/login':
+                res = self._paths[path](query_dict, sid)
+            else:
+                res = self._paths[path](query_dict)
         else:
             res = self.default_response(query_dict)
-
-        return res
+        return res + [response_headers]
 
     def register(self, path, callback):
         self._paths[path] = callback
 
     def default_response(self, *args):
-        return 404, "Nooo 404!"
+        return [404, "Nooo 404!", create_response_headers()]
 
 
 class TextController(object):
+
     @staticmethod
-    def index(query_dict):
+    def index(query_dict, sid):
         text = query_dict.get('id', [''])[0]
         text = model.get(text, '')
-
         titles = model.all()
+        need_auth = 0
+        user_stat = sess.get(sid)
+        amount_of_viewed_pages = user_stat.get('amount_of_viewed_pages')
+
+        if (amount_of_viewed_pages == 3) & (user_stat.get('is_auth') == 0):
+            text = ''
+            need_auth = 1
+        elif (amount_of_viewed_pages < 3) & (text != ''):
+            amount_of_viewed_pages += 1
+            sess.set(sid, {
+                'amount_of_viewed_pages': amount_of_viewed_pages,
+                'is_auth': user_stat.get('is_auth')
+            })
         context = {
             'titles': titles,
             'text': text,
+            'auth': need_auth,
+            'is_auth': user_stat.get('is_auth')
         }
-
-        return 200, view_text.render(context)
+        return [200, view_text.render(context)]
 
     @staticmethod
     def add(query_dict):
@@ -100,7 +165,17 @@ class TextController(object):
         value = query_dict.get('v', [''])[0]
         model.set(key, value)
         context = {'url': "/text"}
-        return 200, view_redirect.render(context)
+        return [200, view_redirect.render(context)]
+
+    @staticmethod
+    def login(query_dict, sid):
+        code = query_dict.get('code', [''])[0]
+        user_stat = sess.get(sid)
+        if code == "1234":
+            user_stat['is_auth'] = 1
+            sess.set(sid, user_stat)
+        context = {'url': "/text"}
+        return [200, view_redirect.render(context)]
 
 
 # ===========================
@@ -112,16 +187,33 @@ class TextController(object):
 class TextView(object):
     @staticmethod
     def render(context):
+        t = ''
+        if context['auth'] == 1:
+            t = """
+            <div style="color: red">Enter the code</div>
+            <form method="GET" action="/text/login">
+                <input type=password name=code />
+                <input type=submit value=LogIn />
+            </form>
+            """
+        else:
+            if context['is_auth'] == 1:
+                t = """
+                Now you can read more articles!
+                """
+            t += """
+            <form method="GET">
+                <input type=text name=id />
+                <input type=submit value=read />
+            </form>
+            """
+
         context['titles'] = [
             '<li>{0}</li>'.format(x) for x in context['titles']
         ]
         context['titles'] = '\n'.join(context['titles'])
 
-        t = """
-        <form method="GET">
-            <input type=text name=id />
-            <input type=submit value=read />
-        </form>
+        t += """
         <form method="GET" action="/text/add" >
             <input type=text name=k /> <input type=text name=v />
             <input type=submit value=write />
@@ -144,17 +236,17 @@ class RedirectView(object):
 #          Main
 #
 # ===========================
-
+sess = SessionModel()
 rout = Router()
 model = TextModel()
 view_text = TextView()
 view_redirect = RedirectView()
 controller = TextController()
 
-rout.register('/', lambda x: (200, "Index HI!"))
+rout.register('/', lambda x: ([200, "Index HI!"]))
 rout.register('/text', controller.index)
 rout.register('/text/add', controller.add)
-
+rout.register('/text/login', controller.login)
 
 # ===========================
 #
@@ -162,11 +254,9 @@ rout.register('/text/add', controller.add)
 #
 # ===========================
 
-def application(environ, start_response):
-    http_status_code, response_body = rout.route(environ, start_response)
-    # response_body += '<br><br> The request ENV: {0}'.format(repr(environ))
-    http_status_code_and_msg = http_status(http_status_code)
-    response_headers = [('Content-Type', 'text/html')]
 
+def application(environ, start_response):
+    http_status_code, response_body, response_headers = rout.route(environ, start_response)
+    http_status_code_and_msg = http_status(http_status_code)
     start_response(http_status_code_and_msg, response_headers)
-    return [response_body]  # it could be any iterable.
+    return [response_body]
